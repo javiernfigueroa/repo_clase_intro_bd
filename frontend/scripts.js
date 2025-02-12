b29d677614797b7938a1cd7c552fb52a8df6d92d
Javier Figueroa <javiernfigueroa@gmail.com>
2025-02-12 19:55:22 -0300

    update

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,9 +1,10 @@
 const btnCarga = document.getElementById('btnCargar'); // Corregido
 const listaAmigos = document.getElementById('listaAmigos');
 
+//mostrar usuarios
 
 btnCarga.addEventListener('click', async () => {
-    const response = await fetch('http://localhost:3000/amigos');
+    const response = await fetch('http://localhost:3000/usuarios');
     const amigos = await response.json()
 
     listaAmigos.innerHTML = '';
@@ -14,3 +15,25 @@ btnCarga.addEventListener('click', async () => {
         listaAmigos.appendChild(li)
     })
 })
+
+
+//agregar usuarios
+
+formAgregar.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const nombre = document.getElementById('nombre').value;
+    const prioridad = document.getElementById('prioridad').value;
+
+    const response = await fetch('http://localhost:3000/agregarUsuario', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, nombre, prioridad: parseInt(prioridad) })
+    });
+    const nuevoUsuario = await response.json();
+    alert(`Usuario ${nuevoUsuario.nombre} agregado con éxito`);
+
+    formAgregar.reset();
+    btnCarga.click();
+});
